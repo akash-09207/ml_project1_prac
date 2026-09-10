45d3655f23003b85706390120e9b916b40828609
akash0920-jpg <thelegendakash9@gmail.com>
2026-09-11 03:16:50 +0530

    Did HyperParameter tunning and also deployed on  flask app, made perdiction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,11 @@
 import os,sys
-import dill
+import dill,pickle
 
 import pandas as pd
 import numpy as np
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,object):
     try:
@@ -16,12 +17,27 @@ def save_object(file_path,object):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_model(x_train,y_train,x_test,y_test,models):
+def evaluate_model(x_train,y_train,x_test,y_test,models,param):
     report={}
     for model in models.items():
         mod=model[1]
+        para=param[model[0]]
+
+        gs = GridSearchCV(mod,para,cv=3,n_jobs=-1)
+        gs.fit(x_train,y_train)
+
+        mod.set_params(**gs.best_params_)
         mod.fit(x_train,y_train)
+        # mod.fit(x_train,y_train)
         y_pred=mod.predict(x_test)
         score=r2_score(y_test,y_pred)
         report[model[0]]=score
     return report
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
